fa3db4afb29835b0ef896baf485032007e5233a6
Vedha Varshini <152283972+vedha-varshini@users.noreply.github.com>
2025-06-13 13:07:49 +0530

    Delete ruby directory

--- ruby/hello.rb
+++
@@ -1,6 +0,0 @@
-var_name = "john"
-var_age = 35
-puts "hello"
-puts ("there once was a man named" + var_name)
-puts ("he was " + var_age.to_s + " years old")
-puts var_name
--- ruby/modules.rb
+++
@@ -1,5 +0,0 @@
-require_relative "tools_mod.rb"
-include Tools
-
-Tools.sayhi("jim")
-Tools.saybye("jim")
